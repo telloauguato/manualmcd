c1b523904ec905837794e3bce221dbcb7c1115be
augusto.marcelo@yahoo.com.br <augusto.marcelo@yahoo.com.br>
2023-07-19 12:45:17 -0300

    Config

--- theme.config.tsx
+++ theme.config.tsx
@@ -15,15 +15,6 @@ const config: DocsThemeConfig = {
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="pt-BR" />
-      <meta name="description" content="𝗣𝗘𝗡𝗦𝗔𝗠𝗘𝗡𝗧𝗢𝗦 𝗘 𝗜𝗗𝗘𝗜𝗔𝗦" />
-      <meta name="og:description" content="𝗣𝗘𝗡𝗦𝗔𝗠𝗘𝗡𝗧𝗢𝗦 𝗘 𝗜𝗗𝗘𝗜𝗔𝗦" />
-      <meta name="twitter:card" content="https://morkgalarhan.vercel.app/morkgalarhan.png" />
-      <meta name="twitter:image" content="https:// morkgalarhan.vercel.app/morkgalarhan.png" />
-      <meta name="twitter:site:domain" content="morkgalarhan.vercel.app" />
-      <meta name="twitter:url" content="https://morkgalarhan.vercel.app" />
-      <meta name="og:title" content="𝗠𝗢𝗥𝗞 𝗚𝗔𝗟𝗔𝗥𝗛𝗔𝗡" />
-      <meta name="og:image" content="https://morkgalarhan.vercel.app/cover.png" />
-      <meta name="apple-mobile-web-app-title" content="𝗠𝗢𝗥𝗞 𝗚𝗔𝗟𝗔𝗥𝗛𝗔𝗡 — 𝗣𝗘𝗡𝗦𝗔𝗠𝗘𝗡𝗧𝗢𝗦 𝗘 𝗜𝗗𝗘𝗜𝗔𝗦" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -56,7 +47,6 @@ const config: DocsThemeConfig = {
       <meta name="msapplication-TileImage" content="/cover.png" />
     </>
   ),
-  search: true,
   prevLinks: true,
   nextLinks: true,
   unstable_faviconGlyph: '🍔'
