d95bfdfe3877529b3a986e6e0cad8f9eddfdec6d
augusto.marcelo@yahoo.com.br <augusto.marcelo@yahoo.com.br>
2023-07-19 12:48:11 -0300

    Config

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,42 +11,6 @@ const config: DocsThemeConfig = {
     text: `Manual McD ${new Date().getFullYear()} — by @telloauguato`,
   },
   titleSuffix:' — Manual McD',
-  head: (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="pt-BR" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/cover.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/cover.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/cover.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/cover.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/cover.png"
-      />
-      <meta name="msapplication-TileImage" content="/cover.png" />
-    </>
-  ),
   prevLinks: true,
   nextLinks: true,
   unstable_faviconGlyph: '🍔'
